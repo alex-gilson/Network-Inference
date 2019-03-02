f0b8d2129d27d516ec69711faa86584d9597e66c
gilson15 <ag4015@ic.ac.uk>
2019-03-02 14:51:09 +0000

    Initial commit

--- izhikevichNetworkSimulation.py
+++ izhikevichNetworkSimulation.py
@@ -5,18 +5,26 @@ print('Running Brian Simulator...')
 
 import sys
 import numpy
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from brian2  import *
 from utility import *
 
 # extract command line arguments
-seed=int(sys.argv[1])
-N=int(sys.argv[2])
-sparsity=float(sys.argv[3])
-simulation_duration=int(sys.argv[4])
-networkFileName=sys.argv[5]
-firingsFileName=sys.argv[6]
-indicesFileName=sys.argv[7]
+# seed=int(sys.argv[1])
+# N=int(sys.argv[2])
+# sparsity=float(sys.argv[3])
+# simulation_duration=int(sys.argv[4])
+# networkFileName=sys.argv[5]
+# firingsFileName=sys.argv[6]
+# indicesFileName=sys.argv[7]
+
+seed=int(2)
+N=int(10)
+sparsity=float(0.1)
+simulation_duration=int(500)
+networkFileName='network.csv'
+firingsFileName='firings.csv'
+indicesFileName='indices.csv'
 
 # set the default seed
 devices.device.seed(seed)
@@ -30,14 +38,13 @@ indices=[]
 tau=1*ms
 
 eqs= '''
-dv/dt = (0.04*v*v + 5*v + 140 - u + I + stimulation)/tau : 1
+dv/dt = (0.04*v*v + 5*v + 140 - u + I)/tau : 1
 du/dt = (a*(b*v-u))/tau : 1
-I = 12*randn() : 1 (constant over dt)
+I = 2*randn() : 1 (constant over dt)
 a:1
 b:1
 c:1
 d:1
-stimulation:1
 '''
 
 threshold='v>30'
@@ -60,24 +67,25 @@ S = Synapses(G, G, 'w:1', on_pre='v_post += w')
 S.connect(condition='i!=j', p=sparsity)
 S.w='30*rand()'
 
-# run the stimulation N number of times
-# at each iteration, stimulate a different node
-for stimulated_node in range(0,N):
-	# reset v and u values
-	G.v=-65
-	G.u=G.b*G.v
+import pdb; pdb.set_trace()
 
-	# only stimulate 1 node
-	G.stimulation = 0
-	G.stimulation[stimulated_node] = 3
+spikemon = SpikeMonitor(G)
 
-	spikemon = SpikeMonitor(G)
+run(500*ms)
+plot(M.t/ms, M.v[0])
+# for t in spikemon.t:
+#     axvline(t/ms, ls='--', c='blue', lw=3)
+# axhline(0.8, ls=':', c='blue', lw=3)
+xlabel('Time (ms)')
+ylabel('Membrane potential (mv)')
+grid()
+title('Fast Spiking neuron with constant imput = 5')
+print("Spike times: %s" % spikemon.t[:])
+show()
 
-	run(simulation_duration*ms)
-
-	# store firings and indices
-	firings.append(list(spikemon.i))
-	indices.append(list(spikemon.t/ms))
+# store firings and indices
+firings.append(list(spikemon.i))
+indices.append(list(spikemon.t/ms))
 
 
 # Write Network to File
--- test.m
+++
@@ -1,5 +0,0 @@
-function [a, b] = test(x,y)
-x = 0
-a = zeros(5,5);
-b = ones(5,5);
-
