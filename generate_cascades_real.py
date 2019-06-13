68f6b97ea2c47213c0aa10a2bb6710c7c46de556
Alejandro Gilson <ag4015@eews506a-044.ee.ic.ac.uk>
2019-06-13 12:30:35 +0100

    Changes lvl 5

--- generate_cascades_real.py
+++ generate_cascades_real.py
@@ -1,14 +1,16 @@
 import scipy.io
 import numpy as np
+import os
 
-n = 4   # Dataset number
+d = 4   # Dataset number
 # Load the data
-filename = 'CRCNS/data/DataSet' + str(n) + '.mat'
+filename = 'CRCNS/data/DataSet' + str(d) + '.mat'
 data = scipy.io.loadmat(filename)['data']
 NUMBER_NEURONS = data['nNeurons'][0][0][0][0]
 N = NUMBER_NEURONS
 simulation_duration = data['recordinglength'][0][0][0][0]
 diffusion_type = 'rayleigh'
+cascadeOption='maximum_cascades'
 
 horizon = 20
 
@@ -37,50 +39,64 @@ sort_idx = np.argsort(firings)
 firings = [firings[sort_idx[i]] for i in range(len(firings))]
 indices = [indices[sort_idx[i]] for i in range(len(indices))]
 
+
 n = 0
 m = 0
 x = 0
 start = firings[n]
 cascades = np.ones((int(simulation_duration/horizon),N))*(-1)
 
-# Iterate through the simulation spikes
-while n < len(firings):
-    
-    if n != 0 and (n > x*1000):
-        print(x*1000)
-        x = x + 1
+if cascadeOption == 'maximum_cascades' or cascadeOption == 'maximum_independence': 
 
-    # define the window of observation
-    start = firings[n] 
-    end = start + horizon
-
-    index = np.array(np.where((firings >= start) & (firings < end)))[0]
-
-    firings_in_window = [firings[i] for i in index]
-    firings_in_window = firings_in_window - start
-    indices_in_window = [indices[i] for i in index]
-
-    # initialise a cascade, non-fired with -1; assume all non-fired
-    current_cascade = np.ones(NUMBER_NEURONS)*(-1)
-
-    # first neuron to spike starts the cascade
-    current_cascade[indices_in_window[0]-1] = 0
-
-    # update cascade based on the rest of the firings
-    for k in range(len(indices_in_window)):
+    # Iterate through the simulation spikes
+    while n < len(firings):
         
-        # Only the first firing is taken into account
-        if current_cascade[indices_in_window[k]-1] == -1:
-            current_cascade[indices_in_window[k]-1] = firings_in_window[k]    
+        if n != 0 and (n > x*10):
+            print(x*10)
+            x = x + 1
 
-    cascades[m] = current_cascade
-    m = m + 1 
+        # define the window f observation
+        start = firings[n] 
+        end = start + horizon
 
-    # The index of the firing that starts the next cascade is the one following the last firing of this cascade 
-    n = index[-1] + 1
+        # Only count the cascade if it had previously reached a steady state
+        if cascadeOption == 'maximum_independence':
+            if n > 0 and np.diff(firings)[n-1] < horizon:
 
-# Delete the cascades with no entries (they were initialized with all -1s)
-cascades = cascades[0:np.where(cascades == 0)[0][-1] + 1]
+               n = n + 1
+               continue 
+
+        index = np.array(np.where((firings >= start) & (firings < end)))[0]
+
+        firings_in_window = [firings[i] for i in index]
+        firings_in_window = firings_in_window - start
+        indices_in_window = [indices[i] for i in index]
+
+        # initialise a cascade, non-fired with -1; assume all non-fired
+        current_cascade = np.ones(N)*(-1)
+
+        # first neuron to spike starts the cascade
+        current_cascade[indices_in_window[0]-1] = 0
+
+        # update cascade based on the rest of the firings
+        for k in range(len(indices_in_window)):
+            
+            # Only the first firing is taken into account
+            if current_cascade[indices_in_window[k]-1] == -1:
+                current_cascade[indices_in_window[k]-1] = firings_in_window[k]    
+
+        # Don't count the cascades where only one node fires
+        if np.bincount(current_cascade.astype(int) + 1)[0] < N - 1:
+            cascades[m] = current_cascade
+            m = m + 1 
+
+        # The index of the firing that starts the next cascade is the one following the last firing of this cascade 
+        n = index[-1] + 1
+
+    # Delete the cascades with no entries (they were initialized with all -1s)
+    cascades = cascades[0:np.where(cascades == 0)[0][-1] + 1]
+    # print('Number of cascades: ', m)
+
 
 x = 0
 
@@ -104,7 +120,8 @@ for c in range(len(cascades)):
     # stimulated node)
     for i in range(1,len(val)):
         
-        # num_cascades stores the amount of times each node has fired
+        # num_cascades stores the amount of times each node has generated a cascade
+        # num_cascades = [np.bincount(cascades.T[k].astype(int)+1)[1] for k in range(cascades.shape[1])]
         num_cascades[idx[order[i]]] = num_cascades[idx[order[i]]] + 1
 
         for j in range(i-1):
@@ -134,15 +151,20 @@ for c in range(len(cascades)):
                     A_bad[idx[order[i]], j] = A_bad[idx[order[i]], j] + 0.5*(horizon-val[i])**2;
             
         
-        
-np.savetxt("w/a_potential.csv", A_potential, delimiter=",")
-np.savetxt("w/a_bad.csv", A_bad, delimiter=",")
-np.savetxt("w/cascades.csv", cascades, delimiter=",")
-np.savetxt("w/num_cascades.csv", num_cascades, delimiter=",")
+try: 
+    foldername = "r/CRCNS_" + str(d) + "/"
+    np.savetxt(foldername + "a_potential_" + cascadeOption  + ".csv", A_potential, delimiter=",")
+    np.savetxt(foldername + "a_bad.csv_" + cascadeOption  + ".csv", A_bad, delimiter=",")
+    np.savetxt(foldername + "cascades_" + cascadeOption  + ".csv", cascades, delimiter=",")
+    np.savetxt(foldername + "num_cascades_" + cascadeOption  + ".csv", num_cascades, delimiter=",")
+
+except Exception as e:
+    print(e)
 
 
 print('Finished generating cascades')
 
+os.system('poweroff')
 
 
 
